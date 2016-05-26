63e46095117b7f53638348acb2fbe573850a4b91
kudotty <kudotty@users.noreply.github.com>
2016-05-26 15:43:47 +0900

    [+] Use scrapy_rethinkdb.RethinkDBPipeline

--- scrapy_tutorial/settings.py
+++ scrapy_tutorial/settings.py
@@ -64,9 +64,12 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'scrapy_tutorial.pipelines.SomePipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'scrapy_rethinkdb.RethinkDBPipeline': 300,
+}
+RETHINKDB_TABLE_MAPPING = {
+    'Item': 'items',
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
